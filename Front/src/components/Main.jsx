337699df9ce1a75afaf2eb71cbe1152c4b45effb
Diesos <ozturk@live.Fr>
2024-08-17 00:49:26 +0200

    refactor: Update RandomExcuse service to use correct API endpoint

--- Front/src/components/Main.jsx
+++ Front/src/components/Main.jsx
@@ -10,8 +10,10 @@ const Wrapper = styled.section`
 `;
 const Main = () => {
   const [buttonTime, setButtonTime] = useState(false);
-  const [excuse, setExcuse] = useState([]);
+  const [excuse, setExcuse] = useState('');
+  const [tag, setTag] = useState('');
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -20,27 +22,36 @@ const Main = () => {
     return () => clearTimeout(timer);
   }, []);
 
-  useEffect(() => {
-    const fetchExcuse = async () => {
-      try {
+
+  const fetchExcuse = async () => {
+    try {
+      let newExcuse;
+      let newTag;
+      do {
         const data = await RandomExcuse();
         if (data.succes) {
-          setExcuse(data.message);
+          newExcuse = data.data.message;
+          newTag = data.data.tag;
+        } else {
+          setError(`Error: ${data.message}`);
+          return;
         }
-      } catch (error) {
-        setError("Error fetching activities" + error.message);
-      }
-    };
+      } while (newExcuse === excuse); // Continue à fetcher jusqu'à obtenir une nouvelle excuse
 
-    fetchExcuse();
-  }, [excuse]);
+      setExcuse(newExcuse); // Mettre à jour l'état avec la nouvelle excuse
+      setTag(newTag); // Mettre à jour l'état avec la nouvelle excuse
+      setLoading(false)
+    } catch (error) {
+      setError("Error fetching activities: " + error.message);
+    }
+  };
 
   return (
     <Wrapper>
       <Header />
       <div className="bubble shadow large bottom">
         <TypeAnimation
-          sequence={["Bonjour, je suis un DEV, et j'ai un soucis..", 3000]}
+          sequence={["Bonjour, peut-tu m'aider à trouver une excuse ?", 3000]}
           wrapper="span"
           speed={50}
           style={{ fontSize: "2em", display: "inline-block" }}
@@ -48,11 +59,20 @@ const Main = () => {
           className="animated-text"
         />
         {buttonTime && (
-          <button className="generate-btn" onClick={RandomExcuse}>
+          <button className="generate-btn" onClick={fetchExcuse}>
             Generate an Excuse
           </button>
         )}
-        {buttonTime && <div>{error ? <p>{error}</p> : <p>{excuse}</p>}</div> }
+        <div>
+          {loading && <p></p>}
+          {error && <p>{error}</p>}
+          {!loading && !error && (
+            <>
+              <p>Object: {tag}</p>
+              <p>Excuse: {excuse}</p>
+            </>
+          )}
+        </div>
       </div>
       <img className="hero" src={myImage}></img>
     </Wrapper>
--- Front/src/services/RandomExcuse.jsx
+++ Front/src/services/RandomExcuse.jsx
@@ -2,7 +2,7 @@ import axios from './axios'
 
 const RandomExcuse = async() => {
 	try {
-		const res = await axios.get("http://localhost/excuse/random/")
+		const res = await axios.get("http://localhost:8000/excuse/random/")
 		return res.data;
 	} catch(error) {
 		console.error('Error fetching excuses', error);
