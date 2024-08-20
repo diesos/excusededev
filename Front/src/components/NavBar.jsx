b56e4b41aafe940b2219c7fbeeed7b0be904f2a4
Diesos <ozturk@live.Fr>
2024-08-21 00:39:33 +0200

    refactor: Update NavBar component to use styled-components and improve code readability

--- Front/src/components/NavBar.jsx
+++ Front/src/components/NavBar.jsx
@@ -1,55 +1,53 @@
-import styled, { css } from 'styled-components'
-import { Link, useLocation } from 'react-router-dom';
+import styled from "styled-components";
+import { Link, useLocation } from "react-router-dom";
 
 const HeadNav = styled.h1`
-    font-size: clamp(20px, 4vw, 30px);
-    font-family: "Poppins", sans-serif;
-    padding: clamp(15px, 5vw, 30px);
-    margin-left: clamp(5px, 5vw, 10%);
+  font-size: clamp(20px, 4vw, 30px);
+  font-family: "Poppins", sans-serif;
+  padding: clamp(15px, 5vw, 30px);
+  margin-left: clamp(5px, 5vw, 10%);
 `;
+
 const NavContainer = styled.nav`
-	display: flex;
-	justify-content: space-between;
-	font-family: "Poppins";
+  display: flex;
+  justify-content: space-between;
+  font-family: "Poppins";
+`;
 
-`
-const NavBtn = styled.button `
-	background-color: transparent;
-	border-radius: 8px;
-	padding: 10px;
-	color:white;
+const NavBtn = styled.button`
+  background-color: transparent;
+  border-radius: 8px;
+  padding: 10px;
+  color: white;
+  border: none;
+  cursor: pointer;
+`;
 
-`
+const NavBar = ({ setAddExcuse }) => {
+  const location = useLocation();
 
+  // Check if the current path is not '/'
+  if (location.pathname !== "/") return null;
 
+  return (
+    <NavContainer>
+      <HeadNav>Excuse De Dev</HeadNav>
+      <ul>
+        <li>
+          <NavBtn
+            onClick={(e) => {
+              e.preventDefault();
+              setAddExcuse((value) => !value);
+            }}
+          >
+            <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+              Ajouter/Générer Une Excuse
+            </Link>
+          </NavBtn>
+        </li>
+      </ul>
+    </NavContainer>
+  );
+};
 
-const NavBar = ({setAddExcuse}) => {
-
-	if (location.pathname != '/')
-		return;
-
-
-	return(
-		<NavContainer>
-			<HeadNav>
-				Excuse De Dev
-			</HeadNav>
-				<ul>
-				<li>
-					<a onClick={(e) => {
-					e.preventDefault();
-					setAddExcuse(Value => !Value)
-					}}><Link to="/">
-						<NavBtn>
-							Ajouter/Générer Une Excuse
-						</NavBtn>
-						</Link>
-						</a>
-				</li>
-			</ul>
-			</NavContainer>
-	)
-
-}
-
-export default NavBar
+export default NavBar;
--- back/config/corsOption.js
+++ back/config/corsOption.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://diesos-excusededev.netlify.app/'],
+    origin: 'http://localhost:5173',
     methods: 'GET, POST',
     credentials: true,
   };
