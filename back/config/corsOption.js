4c537fc6872052b358a4033f0e9b4315c7c26f5e
Omer Ozturk <45888058+diesos@users.noreply.github.com>
2024-08-20 23:18:06 +0200

    Update corsOption.js

--- back/config/corsOption.js
+++ back/config/corsOption.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://diesos-excusededev.netlify.app/'],
     methods: 'GET, POST',
     credentials: true,
   };
