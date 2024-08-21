ae9a0573f9bf206e41325940b1e8e964f3931e1d
Diesos <ozturk@live.Fr>
2024-08-21 16:51:00 +0200

    chore: Update API base URL to use HTTPS

--- Front/src/services/axios.jsx
+++ Front/src/services/axios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://excusededev.onrender.com',
   withCredentials: false
 });
 
