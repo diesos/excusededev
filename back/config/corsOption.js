d8a91f7e18efe643feb6c8557572aadd6fc957b3
Diesos <ozturk@live.Fr>
2024-08-21 16:15:13 +0200

    chore: Update CORS options to allow multiple origins

--- back/config/corsOption.js
+++ back/config/corsOption.js
@@ -1,5 +1,9 @@
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: [
+      "http://localhost:5173",
+      "https://main--diesos-excusededev.netlify.app/",
+      "https://diesos-excusededev.netlify.app/",
+    ],
     methods: 'GET, POST',
     credentials: true,
   };
